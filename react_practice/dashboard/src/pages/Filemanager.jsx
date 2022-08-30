0ac32e9491fdb0b7547bcb14829d01d89dbec79f
Subhocr07 <96890352+Subhocr07@users.noreply.github.com>
2022-08-30 13:22:01 +0530

    sidebar completed

---
+++ react_practice/dashboard/src/pages/Filemanager.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Filemanager = () => {
+  return (
+    <div style={{background:"red",height:"100vh",textAlign:'center'}}>
+      FileManager
+    </div>
+  )
+}
+
+export default Filemanager
